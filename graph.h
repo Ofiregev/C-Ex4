052bb15ab07c6b69fb7b9eb8f32735af2fcf65e1
ofiregev <ofirmeidan1998@gmail.com>
2021-12-26 11:24:19 +0200

    commit

---
+++ graph.h
@@ -0,0 +1,45 @@
+#ifndef GRAPH_
+#define GRAPH_
+
+
+
+typedef struct GRAPH_NODE *pnode;
+
+typedef struct GRAPH_EDGE{
+    int weight;
+    pnode dest;
+    struct GRAPH_EDGE *next;
+} edge, *pEdge;
+
+typedef struct GRAPH_NODE{
+    int id;
+    pEdge edges;
+    struct GRAPH_NODE * next;
+} node , *pnode;
+
+void deleteFirst(pEdge *head);
+void removeByid(pEdge * head,int dest);
+void removeALLedges(pEdge *head);
+void removeEdge(pEdge *prev);
+
+void insert_edge(pEdge *head,pnode dest, int w);
+void get_edge(pnode *head, pnode* src);
+
+pnode find_node(int id, pnode head);
+pnode addNOdeB (pnode *head);
+pnode addNode (pnode *head);
+
+void build_graph_cmd(pnode *pn);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- main.c
+++ main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-typedef struct GRAPH_NODE *pnode;
-
-typedef struct GRAPH_EDGE{
-    int weight;
-    pnode dest;
-    struct GRAPH_EDGE *next;
-} edge, *pEdge;
-
-typedef struct GRAPH_NODE{
-    int id;
-    pEdge edges;
-    struct GRAPH_NODE * next;
-} node , *pnode;
+#include "graph.h"
 
 pnode find_node(int id, pnode head)
 {
@@ -27,6 +13,31 @@ pnode find_node(int id, pnode head)
         head = head->next;
     }
     return NULL;
+}
+
+pnode addNOdeB (pnode *head)
+{
+int id;
+    printf("please enter node id");
+    if(scanf("%d",&id)==0)
+    {
+        printf("type integer value");
+        return NULL;
+    }
+    pnode ptn= find_node(id, *head);
+    if(ptn==NULL)
+    {
+        ptn = (pnode) malloc(sizeof(node));
+        if(ptn==NULL)
+        {
+            printf("eroor");
+            return ptn;
+        }
+        ptn->id =id;
+        ptn->next = *head;
+        *head =  ptn;
+    }
+    return ptn;
 }
 
 
@@ -105,21 +116,80 @@ void build_graph_cmd(pnode *pn)
     if (x == 'n')
     {
        
-        pnode src = addNode(pn);
-        pnode src2 = addNode(pn);
-        get_edge(pn,&src);
-        get_edge(pn,&src);
-        get_edge(pn,&src2);
-        get_edge(pn,&src2);
+        pnode src1 = addNode(pn);//1
+        pnode src2 = addNode(pn);//2
 
+        get_edge(pn,&src1);//2
+        get_edge(pn,&src2);//3
+        get_edge(pn,&src2);//4
+        get_edge(pn,&src2);//1
 
-        
+        pnode p = *pn;
+        while (p)
+        {
+            removeByid(&(p->edges),src2->id);
+            p = p->next;
+        }
+        removeALLedges(&(src2->edges));  
     }
    
     if(*pn==NULL)
     {
         printf("not good");
     }
+}
+
+void removeALLedges(pEdge *head)
+{
+    while(*head)
+    {
+        deleteFirst(head);
+    }
+}
+
+void deleteFirst(pEdge *head){
+if(!*head) return;
+pEdge p =*head;
+*head = p->next;
+free(p);
+}
+
+void removeEdge(pEdge *prev)
+{
+    pEdge p = (*prev)->next;
+    (*prev)->next = (*prev)->next->next;
+    free(p);
+}
+
+void removeByid(pEdge * head,int dest)
+{
+    if((*head)== NULL)
+    {
+        return;
+    }
+    if((*head)->dest->id == dest)
+    {
+        deleteFirst(head);
+        return;
+    }
+    pEdge prev = *head;
+    pEdge p = (*head)->next;
+    
+    while (p)
+    {
+        if(p->dest->id == dest)
+        {
+            removeEdge(&prev);
+        }
+        p = p->next;
+        prev = prev->next;
+
+    }
+    
+
+
+
+
 }
 
 
@@ -132,14 +202,14 @@ int main()
     // addNode(&pn);
     // addNode(&pn);
     // addNode(&pn);
-    while (pn){
-    if (pn->edges!=NULL)
-    {
-        printf("%d ,", pn->id);
-        printf("%d",pn->edges->weight);
-    }
-    pn = pn->next;
-    }
+    // while (pn)
+    // {
+    //     printf("%d ,", pn->id);
+    //     printf("%d",pn->edges->weight);
+    //     pn->edges = pn->edges->next;
+    // }
+    // pn = pn->next;
+    
     
     
     // printf("%d", pn->next->id);
